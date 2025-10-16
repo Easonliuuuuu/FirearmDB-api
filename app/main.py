8124f32b95ebe1f01e06b202a1127e83ecfe05a4
Easonliuuuuu <easoneastin@gmail.com>
2025-10-16 21:18:12 +0800

    test

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ from app.context import _request_ctx_var
 from mangum import Mangum
 
 
-app = FastAPI()
+app = FastAPI(root_path="/Prod")
 
 @app.middleware("http")
 async def request_context_middleware(request: Request, call_next):
